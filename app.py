ef5957e9e7c3ba73b44d773f576364b2380b9464
olliekkvgex <oliverdpickup@outlook.com>
2026-04-27 17:08:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ with st.sidebar:
         st.caption(f"• **{cat['name']}**")
 
 # --- MAIN APP UI ---
-st.set_page_config(page_title="ANUBIS - Book Detective", page_icon="🕵️‍♀️")
+st.set_page_config(page_title="ANUBIS - Book Detective", page_icon="Anubis.png")
 st.title("🕵 ANUBIS - Book Detective")
 
 # --- STEP 1: ISBN INPUT ---
